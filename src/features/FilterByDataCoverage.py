1de8aaf11560aa9a4241d66aa1f4c8a8393f5636
Laura Goon <goon.laura@gmail.com>
2021-03-11 18:36:06 -0500

    filter out indicators via data coverage for a set of indicators

--- src/features/FilterByDataCoverage.py
+++ src/features/FilterByDataCoverage.py
@@ -1,16 +1,32 @@
 import world_bank_data as wb
 
 # get data from API
-#all_indicators = wb.get_indicators()
+all_indicators = wb.get_indicators()
+all_indicators_codes = list(all_indicators.index)
 #all_countries = wb.get_countries()
 
 # Colombia country data
 co_country = wb.search_countries('Colombia')
 
 # indicator data for Colombia
-#indicator_names = 
-pop_df = wb.get_series('SP.POP.TOTL')
-colombia_pop_df = pop_df.loc['Colombia']
-num_na = colombia_pop_df.isna().sum()
-num_rows = len(colombia_pop_df)
-coverage_percentage = (num_rows - num_na) / (num_rows)
+coverage_threshold = 85.0
+keep_indicators = []
+
+for indicator in all_indicators_codes:
+    df = wb.get_series(indicator)
+    colombia_df = df.loc['Colombia'] # only Colombia data
+    num_na = colombia_df.isna().sum()
+    num_rows = len(colombia_df)
+    coverage_percentage = (num_rows - num_na) / num_rows
+    print(coverage_percentage)
+
+    if coverage_percentage >= coverage_threshold: # filter out indicators
+        keep_indicators.append(indicator)
+
+print(keep_indicators)
+
+# pop_df = wb.get_series('SP.POP.TOTL')
+# colombia_pop_df = pop_df.loc['Colombia']
+# num_na = colombia_pop_df.isna().sum()
+# num_rows = len(colombia_pop_df)
+# coverage_percentage = (num_rows - num_na) / (num_rows)
