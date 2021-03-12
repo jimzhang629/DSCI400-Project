cf2bf70ce990297c61ce4e7d8bb78910a4f5b9f5
Laura Goon <goon.laura@gmail.com>
2021-03-11 19:04:58 -0500

    add docstrings and error handling for feature selection by data coverage functions

--- src/FeatureSelection.py
+++ src/FeatureSelection.py
@@ -1,56 +1,43 @@
 import world_bank_data as wb
 
 def get_indicator_codes():
-    all_indicators = wb.get_indicators(topic=3, source=2)
+    '''
+    Get list of code/id's for all indicators.
+    '''
+    all_indicators = wb.get_indicators()
     return list(all_indicators.index)
 
 def get_indicators_for_country(country, threshold=0.0):
+    '''
+    Get list of indicators with data for specified country.
+
+    country -- returned indicators should have data available for this country
+    threshold -- returned indicators should have a data coverage for the
+                 specified country equal to or above this threshold
+    '''
     indicator_codes = get_indicator_codes()
     #country_data = wb.search_countries(country)
     keep_indicators = []
 
     for indicator in indicator_codes:
-        df = wb.get_series(indicator)
-        country_df = df.loc[country] # only country-specific data
+        try:
+            df = wb.get_series(indicator)
+        except ValueError:
+            continue # ignore deleted or archived indicators
+
+        try:
+            country_df = df.loc[country]
+        except KeyError:
+            continue # no relevant country data in this indicator, so ignore
+        
         num_na = country_df.isna().sum()
         num_rows = len(country_df)
         coverage_percentage = (num_rows - num_na) / num_rows
         print(coverage_percentage)
 
-        if coverage_percentage >= threshold: # filter out indicators
+        if coverage_percentage >= threshold:
             keep_indicators.append(indicator)
 
     return keep_indicators
 
 print(get_indicators_for_country('Colombia', 0.85))
-
-# # get data from API
-# all_indicators = wb.get_indicators()
-# all_indicators_codes = list(all_indicators.index)
-# #all_countries = wb.get_countries()
-
-# # Colombia country data
-# co_country = wb.search_countries('Colombia')
-
-# # indicator data for Colombia
-# coverage_threshold = 85.0
-# keep_indicators = []
-
-# for indicator in all_indicators_codes:
-#     df = wb.get_series(indicator)
-#     colombia_df = df.loc['Colombia'] # only Colombia data
-#     num_na = colombia_df.isna().sum()
-#     num_rows = len(colombia_df)
-#     coverage_percentage = (num_rows - num_na) / num_rows
-#     print(coverage_percentage)
-
-#     if coverage_percentage >= coverage_threshold: # filter out indicators
-#         keep_indicators.append(indicator)
-
-# print(keep_indicators)
-
-# pop_df = wb.get_series('SP.POP.TOTL')
-# colombia_pop_df = pop_df.loc['Colombia']
-# num_na = colombia_pop_df.isna().sum()
-# num_rows = len(colombia_pop_df)
-# coverage_percentage = (num_rows - num_na) / (num_rows)
